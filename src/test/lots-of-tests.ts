2a0075920e07151758e68681885ab5ca2db08473
Tyler Murphy <tylermurphyj@gmail.com>
2019-02-03 06:43:25 -0800

    Adds more test tests. One file has many tests. One directory has many files containing tests.

---
+++ src/test/lots-of-tests.ts
@@ -0,0 +1,12 @@
+import test from '../index'
+import * as assert from 'assert'
+
+const randomDelay = () => new Promise(resolve => setTimeout(resolve, Math.round(Math.random() * 1e3)))
+const failOnePercentOfTheTime = () => assert.notEqual(100, Math.ceil(Math.random() * 100))
+
+for (let i = 0; i < 1e5; i += 1) {
+    test(`random delay ${i}`, async () => {
+        await randomDelay()
+        failOnePercentOfTheTime()
+    })
+}
